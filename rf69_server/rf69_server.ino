395bdb5bc07726529beb71ab3faf50a49f59f79c
Joseivaldo Benito Junior <jrbenito@benito.qsl.br>
2014-08-29 22:10:17 -0300

    RH Server example with RTC
    Signed-off-by: Joseivaldo Benito Junior <jrbenito@benito.qsl.br>

--- rf69_server/rf69_server.ino
+++ rf69_server/rf69_server.ino
@@ -12,11 +12,68 @@
 // Tested on Teensy 3.1 with RF69 on PJRC breakout board
 
 #include <SPI.h>
+#include <Wire.h>
+#include <Time.h>
+#include <MCP7940RTC.h>
 #include <RH_RF69.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+
+#define LEDPIN 9 
+
+char buf[32];
+char *p;
+MCP7940RTC *prtc;    // setup rtc pointer
+time_t delta;
 
 // Singleton instance of the radio driver
 RH_RF69 rf69;
 //RH_RF69 rf69(15, 16); // For RF69 on PJRC breakout board with Teensy 3.1
+
+
+// Converts serial message to time for RTC
+#define TIME_SYNC_LEN 11
+time_t processSyncMessage() 
+{
+	static char c;
+	// return the time if message is valid
+	while(Serial.available() >= TIME_SYNC_LEN) {
+		c = Serial.read();
+		if( c == 'T' ) {
+			time_t recvtime =0;
+			for(int i=0; i < TIME_SYNC_LEN -1; i++) {
+				c = Serial.read();
+				if( c >= '0' && c <= '9' ){
+					recvtime = (10 * recvtime) + (c - '0'); // to number
+				}
+			}
+			return recvtime;
+		}
+	}
+	return 0;
+}
+
+void digitalClockDisplay(){
+  p = &buf[0];
+  prtc->getDateStr(p);
+  Serial.print("RTC:  ");
+  Serial.println(p);
+
+  time_t t = now(); // store the current time in time variable t 
+  Serial.print("Time: ");
+  Serial.print(year(t));          // the year for the given time t  
+  Serial.print("-");
+  Serial.print(month(t));         // the month for the given time t 
+  Serial.print("-");
+  Serial.print(day(t));           // the day for the given time t 
+  Serial.print(" ");
+  Serial.print(hour(t));          // returns the hour for the given time t
+  Serial.print(":");
+  Serial.print(minute(t));        // returns the minute for the given time t
+  Serial.print(":");
+  Serial.println(second(t));        // returns the minute for the given time t
+
+}
 
 void setup() 
 {
@@ -46,11 +103,39 @@ void setup()
   rf69.setSyncWords(syncwords, sizeof(syncwords));
   rf69.setEncryptionKey((uint8_t*)"thisIsEncryptKey");
 #endif
+  
+  memset(buf,0,sizeof(buf));
+  prtc = new MCP7940RTC();
+/*  static time_t t = prtc->get();
+  static tmElements_t tm1;
+  breakTime(t,tm1);
+  Serial.print("t=");
+  Serial.println(t);
+  Serial.print(tm1.Year);
+  Serial.print(" ");
+  Serial.print(tm1.Month);
+  Serial.print(" ");
+  Serial.print(tm1.Day);
+  Serial.print(" ");
+  Serial.print(tm1.Wday);
+  Serial.print(" ");
+  Serial.print(tm1.Hour);
+  Serial.print(":");
+  Serial.print(tm1.Minute);
+  Serial.print(":");
+  Serial.println(tm1.Second);
+*/
+
+  p = &buf[0];
+  prtc->getDateStr(p);
+  Serial.println(p);
+  setTime(prtc->getTimeRTC());
+
+  delta = now();
 }
 
 void loop()
 {
-
   if (rf69.available())
   {
     // Should be a message for us now   
@@ -75,5 +160,23 @@ void loop()
       Serial.println("recv failed");
     }
   }
+  if(Serial.available())
+  {
+	  time_t t = processSyncMessage();
+	  if (t > 0) {
+		  prtc->setTimeRTC(t);
+		  setTime(t);
+	  }
+  }
+ 
+//  Serial.println(now());
+  int diff = now() - delta;
+//  Serial.println(diff); 
+//  Serial.println(delta);
+  if (diff > 5) {
+     digitalClockDisplay();
+     delta = now();
+  }
+
 }
 
